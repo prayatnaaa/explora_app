24347075550ad0e7e2fc168cb926d8a13ca764d3
prayatnaaa <tudeprayatna04@gmail.com>
2024-04-10 01:54:32 +0800

    making the login page UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +11,113 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Text("hi");
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Stack(
+              children: [
+                const Align(
+                    alignment: Alignment.centerRight,
+                    child: Image(image: AssetImage("assets/map.png"))),
+                Positioned(
+                  top: 130,
+                  left: 120,
+                  child: Text(
+                    "Welcome back",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 24, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Positioned(
+                  top: 170,
+                  left: 100,
+                  child: Text(
+                    "Sign in to access your account",
+                    style: GoogleFonts.montserrat(fontSize: 14),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            child: TextField(
+              style: GoogleFonts.montserrat(),
+              decoration: const InputDecoration(
+                  hintText: "Enter your email",
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(color: Color(0xFF6C63FF)))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            child: TextField(
+              style: GoogleFonts.montserrat(),
+              decoration: const InputDecoration(
+                  hintText: "Password",
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(color: Color(0xFF6C63FF)))),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            width: 300,
+            height: 50,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color(0xFF6C63FF)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Next",
+                    style: GoogleFonts.montserrat(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xFFFCFCFC)),
+                  ),
+                  const Icon(
+                    Icons.navigate_next,
+                    color: Color(0xFFFCFCFC),
+                  )
+                ],
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "New Member? ",
+                  style: GoogleFonts.montserrat(fontSize: 13),
+                ),
+                Text(
+                  "Register now",
+                  style: GoogleFonts.montserrat(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFF6C63FF)),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
